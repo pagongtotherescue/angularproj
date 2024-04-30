11fc72812f0ddcc6a8d799c3d0a8b790178b811f
Mild Angelee <pagongtotherescue@gmail.com>
2024-04-30 10:58:37 +0800

    UI changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostCreateComponent } from './post/post-create/post-create.component';
+import { PostListComponent } from './post/post-list/post-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: 'create-post', component: PostCreateComponent },
+ { path: 'posts', component: PostListComponent },
+ { path: '', redirectTo: '/posts', pathMatch: 'full' }
+ // Add other routes as needed
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+ imports: [RouterModule.forRoot(routes)],
+ exports: [RouterModule]
 })
 export class AppRoutingModule { }
